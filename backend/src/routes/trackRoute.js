5035fc4f3b53c749e5edd661c8f49225f8ea8c40
Lionel Adams <adamslionel@hotmail.com>
2019-10-02 19:56:48 +0100

    feat: Add post track route

--- backend/src/routes/trackRoute.js
+++ backend/src/routes/trackRoute.js
@@ -13,7 +13,30 @@ router.get('/tracks', async (req, res) => {
   // get all tracks
   const tracks = await Track.find({userId: req.user._id});
 
-  res.send(tracks)
+  res.send(tracks);
+});
+
+router.post('/tracks', async (req, res) => {
+  // create a track
+  const {name, locations} = req.body;
+
+  if(!name || !locations) {
+    return res.status(422).send({error: 'You must provide name and locations'})
+  }
+
+  try {
+    const track = new Track({name, locations, userId: req.user._id});
+    
+    await track.save();
+    
+    res.send(track);
+  } catch (err) {
+    return res.status(422).send({error: error.message})
+    
+}
+
+
+
 })
 
 module.exports = router;
