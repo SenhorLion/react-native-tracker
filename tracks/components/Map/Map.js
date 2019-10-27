e0f019e82dcd34e9219f9241d46e4b2f78bf7a60
Lionel Adams <adamslionel@hotmail.com>
2019-10-27 11:50:55 +0000

    feat: Add currentLocation coordinates and location marker

--- tracks/components/Map/Map.js
+++ tracks/components/Map/Map.js
@@ -1,24 +1,46 @@
 import React, { useContext } from "react";
-import { View, StyleSheet } from "react-native";
-import MapView, { Polyline } from "react-native-maps";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import MapView, { Polyline, Circle } from "react-native-maps";
 
 import { Context as LocationContext } from "../../context/LocationContext";
 
 const Map = () => {
-  const { state } = useContext(LocationContext);
+  const {
+    state: { currentLocation }
+  } = useContext(LocationContext);
 
-  console.log({ state });
+  console.log({ currentLocation });
+
+  if (!currentLocation) {
+    return (
+      <ActivityIndicator
+        size="large"
+        style={{ marginTop: 200 }}
+      ></ActivityIndicator>
+    );
+  }
 
   return (
     <MapView
       style={styles.mapContainer}
       initialRegion={{
-        latitude: 38.5259958,
-        longitude: -8.8958961,
+        ...currentLocation.coords,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-    ></MapView>
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01
+      }}
+    >
+      <Circle
+        center={currentLocation.coords}
+        radius={60}
+        strokeColor="rgba(255,0,255, 1.0)"
+        fillColor="rgba(255,158,255, 0.5)"
+      ></Circle>
+    </MapView>
   );
 };
 {
