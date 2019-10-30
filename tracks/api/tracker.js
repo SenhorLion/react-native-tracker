d7512da63452560e26dacca5f9d3837cd95d643c
Lionel Adams <adamslionel@hotmail.co.uk>
2019-10-30 23:20:15 +0000

    feat: Add auth to Axios requests using interceptors

--- tracks/api/tracker.js
+++ tracks/api/tracker.js
@@ -1,9 +1,27 @@
 import axios from "axios";
+import { AsyncStorage } from "react-native";
 
 // NB: For local testing, use ngrok if connecting to a device
 // const BASE_URL = "http://localhost:3003";
-const BASE_URL = "http://7e46f175.ngrok.io";
+const BASE_URL = "http://b988d33b.ngrok.io";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: BASE_URL
 });
+
+instance.interceptors.request.use(
+  async config => {
+    const token = await AsyncStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
