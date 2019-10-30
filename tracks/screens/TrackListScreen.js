515e9b680c6e67d3238333549673440ba0e25541
Lionel Adams <adamslionel@hotmail.co.uk>
2019-10-30 23:18:53 +0000

    feat: Display List of saved tracks

--- tracks/screens/TrackListScreen.js
+++ tracks/screens/TrackListScreen.js
@@ -1,8 +1,10 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import { Text } from "react-native-elements";
+import React, { useContext, useEffect } from "react";
+import { View, FlatList, StyleSheet, TouchableOpacity } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import { Text, ListItem } from "react-native-elements";
 import { NavButton } from "../components/Buttons";
 import { Container } from "../components/container";
+import { Context as TrackContext } from "../context/TrackContext";
 import screenStyles from "./styles";
 
 // TODO: The token here (and logged)  causes a Invariant Violation: Objects are not valid as a React child (found: object with keys {_40, _65, _55, _72}).
@@ -11,20 +13,36 @@ import screenStyles from "./styles";
 // console.log('my user token', token)
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, getTracks } = useContext(TrackContext);
+
   return (
     <Container>
-      <View style={[screenStyles.item, screenStyles.title]}>
-        <Text h1>Track List</Text>
-      </View>
+      <NavigationEvents onWillFocus={getTracks}></NavigationEvents>
 
-      <NavButton text="Track Detail" routeName="TrackDetail" size={24} />
+      <FlatList
+        data={state}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("TrackDetail", {
+                  _id: item._id
+                })
+              }
+            >
+              <ListItem chevron title={item.name}></ListItem>
+            </TouchableOpacity>
+          );
+        }}
+      ></FlatList>
     </Container>
   );
 };
 
 TrackListScreen.navigationOptions = () => {
   return {
-    header: null
+    title: "Tracks"
   };
 };
 
